1b0cd041cedfb385be25456ccc23703e973763dc
Rohan Gupta <rohangupta569@gmail.com>
2022-07-06 01:53:08 +0530

    Adding Products in firebase

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ class App extends React.Component{
           products: [],
           loading:true
       }
-  }
 
+      this.db = firebase.firestore();
+    }
   componentDidMount() {
     // firebase
     // .firestore()
@@ -39,8 +40,7 @@ class App extends React.Component{
     //     loading: false
     //   })
     // })
-    firebase
-    .firestore()
+    this.db
     .collection('products')
     .onSnapshot((snapshot) => {
       console.log(snapshot);
@@ -125,11 +125,30 @@ class App extends React.Component{
 
     return cartTotal;
   }
+
+  addProduct = () => {
+    this.db
+    .collection('products')
+    .add({
+      img : '',
+      price : 900,
+      qty : 3,
+      titile : 'Washing Machine'
+    })
+    .then((docRef) => {
+      console.log('Product has been added', docRef);
+    })
+    .catch((error) => {
+      console.log('Error :', error);
+    })
+  }
+
   render() {
     const {products,loading} = this.state;
     return (
       <div className="App">
         <Navbar count = {this.getCartCount()} />
+        <button onClick={this.addProduct} style = {{padding:20, fontSize:20}}>Add a product</button>
         <Cart 
         products = {products}
         onIncreaseQuantity = {this.handleIncreaseQuantity}
