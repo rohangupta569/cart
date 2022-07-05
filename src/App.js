6358d72924b7cc4eafea13be3a596d5e70de519c
Rohan Gupta <rohangupta569@gmail.com>
2022-07-06 02:15:26 +0530

    Updating Products in firebase

--- src/App.js
+++ src/App.js
@@ -69,14 +69,27 @@ class App extends React.Component{
       const {products} = this.state;
       const index = products.indexOf(product);
 
-      products[index].qty += 1;
+      // products[index].qty += 1;
 
-      this.setState({
-          //products: products
-          //Means the same as above
-          products
+      // this.setState({
+      //     //products: products
+      //     //Means the same as above
+      //     products
+      // })
+      const docRef = this.db.collection('products').doc(products[index].id);
+
+      docRef
+      .update({
+        qty : products[index].qty + 1
+      })
+      .then(() => {
+        console.log('Updated successfully');
+      })
+      .catch((error)=>{
+        console.log('Error :' , error);
       })
   }
+
   handleDecreaseQuantity = (product) => {
       console.log('Heyy please dec the qty of', product);
       const {products} = this.state;
@@ -86,10 +99,22 @@ class App extends React.Component{
           return;
       }
 
-      products[index].qty -= 1;
+      // products[index].qty -= 1;
 
-      this.setState({
-          products
+      // this.setState({
+      //     products
+      // })
+      const docRef = this.db.collection('products').doc(products[index].id);
+
+      docRef
+      .update({
+        qty : products[index].qty - 1
+      })
+      .then(() => {
+        console.log('Updated successfully');
+      })
+      .catch((error)=>{
+        console.log('Error :' , error);
       })
   }
   handleDeleteProduct = (id) => {
@@ -148,7 +173,7 @@ class App extends React.Component{
     return (
       <div className="App">
         <Navbar count = {this.getCartCount()} />
-        <button onClick={this.addProduct} style = {{padding:20, fontSize:20}}>Add a product</button>
+        {/* <button onClick={this.addProduct} style = {{padding:20, fontSize:20}}>Add a product</button> */}
         <Cart 
         products = {products}
         onIncreaseQuantity = {this.handleIncreaseQuantity}
