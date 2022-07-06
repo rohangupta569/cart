cf2bad07b09aa920cf8eed4475439d853745071f
Rohan Gupta <rohangupta569@gmail.com>
2022-07-06 23:30:57 +0530

    Querying the data

--- src/App.js
+++ src/App.js
@@ -42,6 +42,9 @@ class App extends React.Component{
     // })
     this.db
     .collection('products')
+    // .where('price','==',999)
+    // .where('title','==','Mug')
+    .orderBy('price','desc')
     .onSnapshot((snapshot) => {
       console.log(snapshot);
 
